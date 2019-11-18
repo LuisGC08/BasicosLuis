2400b74153dbb226b0b70797c799236c2d6c9a26
usuario <usuario@10.0.2.15>
2019-11-18 11:11:14 +0100

    Cadena

--- src/basicos.java
+++ src/basicos.java
@@ -5,5 +5,20 @@ public class basicos {
 		// TODO Auto-generated method stub
 
 	}
+	/**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void cadenas() {
 
+      String cadena;
+      cadena="HolaMundo";
+
+        
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        
+        
+        System.out.println(cadena);
+        
+    }
 }
