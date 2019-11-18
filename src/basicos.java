a9acf95d25327b09b5da365dc121a023b7c47816
usuario <usuario@10.0.2.15>
2019-11-18 10:35:27 +0100

    cambio en numerico

--- src/basicos.java
+++ src/basicos.java
@@ -3,7 +3,30 @@ public class basicos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 numericos();  // Llamada al método
 	}
+	  /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
 
 }
